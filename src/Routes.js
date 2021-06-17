aa22aebec910f906a39d8bda4483b02779a9b90c
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-17 16:11:04 +0100

    Change BrowserRouter to HashRouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
 
 function Routes() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/profile" component={Profile} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
